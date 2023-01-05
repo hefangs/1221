4223907aa59c0cba1d7ea338fbb891cb900cd248
何方 <he529564582@163.com>
2023-01-05 21:44:16 +0800

    顶部4个card模块静态页面

---
+++ src/views/dashboard/Card/Detail/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="card-header">
+      <span>{{ title }}</span>
+      <i class="el-icon-info" />
+    </div>
+    <div class="card-content">{{ count }}</div>
+    <div class="card-chart">
+      <slot name="charts" />
+    </div>
+    <div class="card-footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    count: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  span {
+    color: #bbb;
+    font-size: 20px;
+  }
+  i {
+    font-size: 20px;
+    color: #bbb;
+  }
+}
+.card-content {
+  font-size: 30px;
+  padding: 10px;
+}
+.card-chart {
+  padding: 0 0 15px 0;
+}
+.card-footer {
+  padding-top: 15px;
+  border-top: 1px solid #eee;
+  font-size: 18px;
+}
+</style>
---
+++ src/views/dashboard/Card/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="6">
+        <el-card>
+          <Detail title="总销售额" count="126560">
+            <template slot="charts">
+              <span>周同比&nbsp;</span>
+              <i class="el-icon-top" />
+              <span>日同比&nbsp;</span>
+              <i class="el-icon-bottom" />
+            </template>
+            <template slot="footer">
+              <span>日销售&nbsp;5008</span>
+            </template>
+          </Detail>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail title="访问量" count="12306">
+            <template slot="charts">
+              <div class="line" />
+            </template>
+            <template slot="footer">
+              <span>日访问量&nbsp;1234</span>
+            </template>
+          </Detail>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail title="支付笔数" count="88460">
+            <template slot="charts">
+              <span>周同比&nbsp;</span>
+              <i class="el-icon-top" />
+              <span>日同比&nbsp;</span>
+              <i class="el-icon-bottom" />
+            </template>
+            <template slot="footer">
+              <span>日支付笔数&nbsp;5783</span>
+            </template>
+          </Detail>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail title="运营活动效果" count="78">
+            <template slot="charts">
+              <span>周同比&nbsp;</span>
+              <i class="el-icon-top" />
+              <span>日同比&nbsp;</span>
+              <i class="el-icon-bottom" />
+            </template>
+            <template slot="footer">
+              <span>日销售&nbsp;5008</span>
+            </template>
+          </Detail>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Detail from './Detail'
+import echarts from './echarts'
+export default {
+  name: 'Card',
+  components: { Detail }
+}
+</script>
+
+<style scoped>
+.el-icon-top {
+  font-size: 20px;
+  color: green;
+}
+.el-icon-bottom {
+  font-size: 20px;
+  color: red;
+}
+</style>
